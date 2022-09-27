228a2cc7ee22cdd95cf2e9b5e5cc6bd00628b099
EmilBacklund <Emil.backlund@stud.noroff.no>
2022-09-27 14:52:24 +0200

    asd

--- day-2-start/1-post-request-user-logIn/js/script.js
+++ day-2-start/1-post-request-user-logIn/js/script.js
@@ -1,60 +1,60 @@
-import '../style.css'
+import '../style.css';
+import { LOGIN_USER_URL_ENDPOINT } from './settings/api.js';
 
-const logInForm = document.querySelector("#login-form");
+const logInForm = document.querySelector('#login-form');
 
-const email = document.querySelector("#email");
-const emailError = document.querySelector("#emailError");
-const emailErrorNotValid = document.querySelector("#emailErrorNotValid");
+const email = document.querySelector('#email');
+const emailError = document.querySelector('#emailError');
+const emailErrorNotValid = document.querySelector('#emailErrorNotValid');
 
+const password = document.querySelector('#password');
+const passwordError = document.querySelector('#passwordError');
 
-const password = document.querySelector("#password");
-const passwordError = document.querySelector("#passwordError");
+const generalErrorMessage = document.querySelector('#general-error-message');
 
-const generalErrorMessage = document.querySelector("#general-error-message");
+logInForm.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-logInForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+  let isEmail = false;
+  if (email.value.trim().length > 0) {
+    emailError.classList.add('hidden');
+    isEmail = true;
+  } else {
+    emailError.classList.remove('hidden');
+  }
 
-    let isEmail = false;
-    if (email.value.trim().length > 0) {
-        emailError.classList.add("hidden");
-        isEmail = true;
-    } else {
-        emailError.classList.remove("hidden");
-    }
+  let isValidEmail = false;
+  if (email.value.trim().length && validateEmail(email.value) === true) {
+    emailErrorNotValid.classList.add('hidden');
+    isValidEmail = true;
+  } else if (email.value.trim().length && validateEmail(email.value) !== true) {
+    emailErrorNotValid.classList.remove('hidden');
+  }
 
-    let isValidEmail = false;
-    if (email.value.trim().length && validateEmail(email.value) === true) {
-        emailErrorNotValid.classList.add("hidden");
-        isValidEmail = true;
-    } else if (email.value.trim().length && validateEmail(email.value) !== true) {
-        emailErrorNotValid.classList.remove("hidden");
-    }
+  let isPassword = false;
 
-    let isPassword = false;
+  if (password.value.trim().length >= 8) {
+    passwordError.classList.add('hidden');
+    isPassword = true;
+  } else {
+    passwordError.classList.remove('hidden');
+  }
 
-    if (password.value.trim().length >= 8) {
-        passwordError.classList.add("hidden");
-        isPassword = true;
-    } else {
-        passwordError.classList.remove("hidden");
-    }
+  let isFormValid = isEmail && isValidEmail && isPassword;
 
-    let isFormValid = isEmail && isValidEmail && isPassword;
-
-    if (isFormValid) {
-        console.log("Validation SUCCEEDED!!  🥳");
-        const userData = {
-            "email": email.value,
-            "password": password.value
-        }
-
-    } else {
-        console.log("Validation FAILED!! 💩");
-    }
+  if (isFormValid) {
+    console.log('Validation SUCCEEDED!!  🥳');
+    const userData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(LOGIN_USER_URL_ENDPOINT);
+  } else {
+    console.log('Validation FAILED!! 💩');
+  }
 });
 
 function validateEmail(email) {
-    const regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(stud.noroff.no|noroff.no)$/;
-    return email.match(regEx) ? true : false;
+  const regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(stud.noroff.no|noroff.no)$/;
+  return email.match(regEx) ? true : false;
 }
---
+++ day-2-start/1-post-request-user-logIn/js/settings/api.js
@@ -0,0 +1,7 @@
+export { LOGIN_USER_URL_ENDPOINT, USER_LOGIN_URL };
+
+const USER_LOGIN_URL = API_BASE_URL + 'api/v1/social/auth/login';
+
+let LOGIN_USER_URL_ENDPOINT =
+  'https://nf-api.onrender.com/api/v1/social/auth/login';
+// API CALL
